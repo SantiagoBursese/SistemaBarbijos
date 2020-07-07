45c16182d6a1f134126234ae93c0f48a85b670cc
santi <santi@DESKTOP-DMC9U78>
2020-07-07 11:01:04 -0300

    Subo clase cliente

---
+++ Sistema/src/Cliente.java
@@ -0,0 +1,36 @@
+
+public class Cliente {
+	private String direccion;
+	private String apellido;
+	private String nombre;
+	
+	public Cliente(String direccion, String apellido, String nombre) {
+		this.apellido = apellido;
+		this.direccion = direccion;
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
