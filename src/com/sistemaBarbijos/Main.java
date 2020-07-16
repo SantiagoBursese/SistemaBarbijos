ab694f4cf58e1a2c49c36669fd82ea276151220f
SolStrapko <solstrapko@hotmail.com>
2020-07-15 21:55:14 -0300

    Muestro menu. Cambios en clase Sistema.

--- src/com/sistemaBarbijos/Main.java
+++ src/com/sistemaBarbijos/Main.java
@@ -9,36 +9,10 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        /*Reporte reporte = new Reporte();
-        Cliente cliente = new Cliente();
-        cliente.setDireccion("direccion");
-        cliente.setApellido("kope");
-        cliente.setNombre("alejo");
-
-        Pedido pedido = new Pedido();
-        pedido.setFecha(new Date());
-        pedido.setCantidadBabijos(3000);
-        pedido.setCliente(cliente);
-
-
-        RepositioPedidos.iniciarListas();
-
-        RepositioPedidos.agregarPedidoGratis(pedido);
-        RepositioPedidos.agregarPedidoGratis(pedido);
-        RepositioPedidos.agregarPedidoGratis(pedido);
-        RepositioPedidos.agregarPedidoGratis(pedido);
-        RepositioPedidos.agregarPedidoGratis(pedido);
-        RepositioPedidos.agregarPedidoGratis(pedido);
-
-        RepositioPedidos.agregarPedidoPagos(pedido);
-        RepositioPedidos.agregarPedidoPagos(pedido);
-        RepositioPedidos.agregarPedidoPagos(pedido);
-        RepositioPedidos.agregarPedidoPagos(pedido);
-        RepositioPedidos.agregarPedidoPagos(pedido);
-
-
-        reporte.generarReporte();
-*/
+    	Sistema sistema = new Sistema();
+    	while(true) {
+    		sistema.ejecutarMenu();
+    	}
 
 
 
--- src/com/sistemaBarbijos/Sistema.java
+++ src/com/sistemaBarbijos/Sistema.java
@@ -1,89 +1,123 @@
 package com.sistemaBarbijos;
 
+import com.sistemaBarbijos.Reporte.Reporte;
 import com.sistemaBarbijos.Repository.RepositioPedidos;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.Scanner;
 
 public class Sistema {
-    private int minimoPromocion;
+	private int minimoPromocion;
+	public Scanner scanner;
+	public Reporte reporte;
 
     public Sistema() {
         RepositioPedidos.iniciarListas();
+    	this.minimoPromocion=100;
+    	this.reporte = new Reporte();
+    	this.scanner = new Scanner(System.in);
     }
+    
+    public void ejecutarMenu() {    	
+    	int opSeleccionada=0;
+    	System.out.println("Ingrese un número del menú: \n 1-Agregar pedido. \n 2-Generar reporte. \n 3-Cambiar cant. mín. de barbijos en la promo("+this.getMinimoPromocion() +").");
+    	opSeleccionada = this.scanner.nextInt();
+    	this.scanner.nextLine();
+	    ejecutarOpcionSeleccionada(opSeleccionada);
 
-    public void obtenerDatosCompra() {
+	    //this.scanner.close();
+    }
+    
+    public void ejecutarOpcionSeleccionada(int opSeleccionada) {
+	    if( opSeleccionada == 1) this.obtenerDatosCompra();
+	    if( opSeleccionada == 2) this.generarReportes();
+	    if( opSeleccionada == 3) this.setCantMinBarbijosPromo();
+
+    }
+    
+    public void generarReportes() {
+    	this.reporte.generarReporte();
+    }
+    
+    public void setCantMinBarbijosPromo() {
+    	System.out.println("Por favor, ingrese la cantidad mínima de barbijos, para la que se aplica la promoción.");
+    	this.minimoPromocion = this.scanner.nextInt();
+    	this.scanner.nextLine();
+    }
+    
+    public void obtenerDatosCompra(){
         Pedido pedido;
         Cliente cliente;
-        Scanner scanner = new Scanner(System.in);
         String nombre, apellido, direccion, localidad, provincia;
         String respuesta = "S";
         Integer cantidadBarbijos;
-        while (!respuesta.equals("N")) {
+        
+        
+        while (!respuesta.equals("N") ) {
             System.out.println("Ingrese nombre del cliente: ");
-            nombre = scanner.nextLine();
+            nombre = this.scanner.nextLine();
 
             System.out.println("Ingrese apellido del cliente: ");
-            apellido = scanner.nextLine();
+            apellido = this.scanner.nextLine();
 
             System.out.println("Ingrese direccion del cliente: ");
-            direccion = scanner.nextLine();
-
+            direccion = this.scanner.nextLine();
+            
             System.out.println("Ingrese localidad del cliente: ");
-            localidad = scanner.nextLine();
-
+            localidad = this.scanner.nextLine();
+            
             System.out.println("Ingrese provincia del cliente: ");
-            provincia = scanner.nextLine();
-
+            provincia = this.scanner.nextLine();
+            
             System.out.println("Ingrese cantidad de barbijos: ");
-            cantidadBarbijos = scanner.nextInt();
-            scanner.nextLine();
-            cliente = registrarCliente(nombre, apellido, direccion, localidad, provincia);
+            cantidadBarbijos = this.scanner.nextInt();
+            this.scanner.nextLine();
+            cliente= registrarCliente(nombre, apellido, direccion, localidad, provincia);
 
-            pedido = registrarPedido(cliente, cantidadBarbijos);
-
-            aplicaPromocion(pedido, cliente);
-
+            pedido= registrarPedido(cliente, cantidadBarbijos);
+            
+            if( aplicaPromocion(pedido,cliente) ) {
+            	RepositioPedidos.agregarPedidoGratis(pedido);
+            }
+            else{
+            	RepositioPedidos.agregarPedidoPagos(pedido);
+            }
             System.out.println("Desea agregar otro pedido?");
-            respuesta = scanner.nextLine();
+            respuesta = this.scanner.nextLine();
         }
-
-        scanner.close();
-
-        //metodo reportes
 
     }
 
-    public Pedido registrarPedido(Cliente cliente, Integer cantidadBarbijos) {
-        Pedido pedido = new Pedido();
-        Date date = new Date();
-        pedido.setCliente(cliente);
+	public Pedido registrarPedido(Cliente cliente, Integer cantidadBarbijos) {
+		Pedido pedido = new Pedido();
+		Date date = new Date();
+		pedido.setCliente(cliente);
         pedido.setCantidadBabijos(cantidadBarbijos);
         pedido.setFecha(date);
         return pedido;
 
-    }
+	}
 
-    public Cliente registrarCliente(String nombre, String apellido, String direccion, String localidad, String provincia) {
-        Cliente cliente = new Cliente();
-        cliente.setNombre(nombre);
+	public Cliente registrarCliente( String nombre, String apellido, String direccion, String localidad, String provincia) {
+		Cliente cliente = new Cliente();
+		cliente.setNombre(nombre);
         cliente.setApellido(apellido);
         cliente.setDireccion(direccion);
         cliente.setLocalidad(localidad);
         cliente.setProvincia(provincia);
+
         return cliente;
-    }
+	}
 
-
-
-    public void aplicaPromocion(Pedido pedido, Cliente cliente) {
-        if(pedido.getCantidadBabijos() >= minimoPromocion && cliente.getProvincia().equals("Buenos Aires")) {
-            RepositioPedidos.agregarPedidoGratis(pedido);
-        } else {
-            RepositioPedidos.agregarPedidoPagos(pedido);
+    public boolean aplicaPromocion(Pedido pedido, Cliente cliente){
+    	boolean aplica= false;
+        if(pedido.getCantidadBabijos() >= this.minimoPromocion && cliente.getProvincia().equals("Buenos Aires")) {
+        	aplica = true;
         }
+            return aplica;
     }
-
+   
 	public int getMinimoPromocion() {
 		return minimoPromocion;
 	}
@@ -91,6 +125,6 @@ public class Sistema {
 	public void setMinimoPromocion(int minimoPromocion) {
 		this.minimoPromocion = minimoPromocion;
 	}
-
-
+    
+    
 }
